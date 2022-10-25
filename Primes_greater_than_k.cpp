c591898af078ce37b4c4adaa95b290e6180ed8a6
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2022-10-25 23:45:42 +0530

    #include<bits/stdc++.h> using namespace std; bool isPrime(int n){     if(n<2){         return false;     }     for(int i=2;i<=sqrt(n);i++){         if(n%i==0){             return false;         }     }     return true; } int main(){     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++){         cin>>arr[i];     }     int cnt=0,k;     cin>>k;     for( int i=0;i< n;i++){         if(isPrime(arr[i]) && arr[i]>=k){             cnt++;         }     }     cout<<cnt; }

---
+++ Primes_greater_than_k.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int n){
+    if(n<2){
+        return false;
+    }
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int cnt=0,k;
+    cin>>k;
+    for( int i=0;i< n;i++){
+        if(isPrime(arr[i]) && arr[i]>=k){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}
